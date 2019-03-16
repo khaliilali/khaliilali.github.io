9a3404e7f5962183ef0e4a1560ab2dd6df7dacff
Khalil Haji <khalilalihaji@gmail.com>
2019-03-15 23:11:57 -0400

    Testing react app

---
+++ precache-manifest.70d17100ddb4240ac8a4664fb6880184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ba1415e55b91748fb3db",
+    "url": "/static/css/main.ecaf953d.chunk.css"
+  },
+  {
+    "revision": "ba1415e55b91748fb3db",
+    "url": "/static/js/main.4e26fc44.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b5bb98644e00b64437a56134c45d6694",
+    "url": "/index.html"
+  }
+];
